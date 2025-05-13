d367683a28039056d5d8bd448a18f334794cc521
eliasschuetz <eliasschuetz1@gmail.com>
2025-05-13 01:10:05 -0500

    Commented functionality.  Noted lack of use due to front end missing function.

--- higgs-backend/routes/logout.js
+++ higgs-backend/routes/logout.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
+// CURRENTLY UNUSED, NO LOGOUT BUTTON ON FRONTEND.
 router.get('/', (req, res, next) => {
+    // destroys current login session, then regenerates when page reloads.
     req.session.destroy();
     res.status(200);
     res.send();
