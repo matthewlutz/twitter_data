7e6dbecd3888ed39c8ad9790230bf2e8d252f218
eliasschuetz <eliasschuetz1@gmail.com>
2025-05-12 23:14:35 -0500

    Pull data from all interaction tables and merge it to send to front end. No limit or offset implementation due to lack of support. Front end currently crashing when sent data, diagnosing currently.

--- higgs-backend/routes/search.js
+++ higgs-backend/routes/search.js
@@ -5,7 +5,33 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        
+        var list = [];
+        var dat = await db.query(
+            'SELECT t.userId AS userA, r.userId AS userB, r.tweetId AS tweetId, t.timestamp, r.content AS content FROM Reply AS r LEFT JOIN Tweet AS t ON r.tweetId = t.tweetId'
+        );
+        for (let i = 0; i < dat[0].length; i++) {
+            dat[0][i].type = "Reply";
+        };
+        list = dat[0];
+
+        dat = await db.query(
+            'SELECT t.userId AS userA, r.userId AS userB, r.tweetId AS tweetId, t.timestamp FROM Retweet AS r LEFT JOIN Tweet AS t ON r.tweetId = t.tweetId'
+        );
+        for (let i = 0; i < dat[0].length; i++) {
+            dat[0][i].type = "Retweet";
+        };
+        list = list.concat(dat[0]);
+
+        dat = await db.query(
+            'SELECT t.userId AS userA, m.userId AS userB, m.tweetId AS tweetId, t.timestamp FROM Mention AS m LEFT JOIN Tweet AS t ON m.tweetId = t.tweetId'
+        );
+        for (let i = 0; i < dat[0].length; i++) {
+            dat[0][i].type = "Mention";
+        };
+        list = list.concat(dat[0]);
+
+        console.log([list]);
+        res.status(200).send(list);
     } catch (err) {
         console.log(err);
     }
