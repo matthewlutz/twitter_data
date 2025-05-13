bbb0d139141175212d652ea8ff62631e98a818c4
eliasschuetz <eliasschuetz1@gmail.com>
2025-05-13 01:17:24 -0500

    Added a comment to describe function.

--- higgs-backend/routes/wall.js
+++ higgs-backend/routes/wall.js
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.all('*', (req, res, next) => {
+    // rudamentary authentication guard, not for use in an actual production product, but adequate for the scope of this project.
     if (req.session.user) {
         return next();
     }
